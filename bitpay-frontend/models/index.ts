45c76916442f7bced50cf13644feb29b08284ecd
udoka-osaji <udokaosaji324@gmail.com>
2026-05-17 15:20:58 +0100

    add index file for exporting models and types in the frontend

---
+++ bitpay-frontend/models/index.ts
@@ -0,0 +1,11 @@
+export { default as User } from './User';
+export { default as Stream } from './Stream';
+export { default as Transaction } from './Transaction';
+export { Challenge } from './Challenge';
+export { StreamTemplate } from './StreamTemplate';
+
+export type { IUser } from './User';
+export type { IStream } from './Stream';
+export type { ITransaction } from './Transaction';
+export type { IChallenge } from './Challenge';
+export type { IStreamTemplate } from './StreamTemplate';
